5d3575206b43f65830fd18fd1961243ccd451fff
jjoonleo <jjoonleo@gmail.com>
2025-07-16 17:45:10 +0900

    refactor: refactor folder structure

--- src/main/kotlin/kr/co/bookquiz/api/ApiApplication.kt
+++ src/main/kotlin/kr/co/bookquiz/api/ApiApplication.kt
@@ -2,10 +2,12 @@ package kr.co.bookquiz.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import kr.co.bookquiz.api.config.JwtProperties
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 class ApiApplication
-
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
 }
