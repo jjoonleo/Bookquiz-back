bbf0a68a8fb1f813ab4abf8f1921d774a95e60dc
jjoonleo <jjoonleo@gmail.com>
2025-07-17 16:47:44 +0900

    feat: add Author, Book, Illustrator, and Translator entities with relationships

---
+++ src/main/kotlin/kr/co/bookquiz/api/entity/Book.kt
@@ -0,0 +1,64 @@
+package kr.co.bookquiz.api.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import com.sun.java.accessibility.util.Translator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import kr.co.bookquiz.api.entity.Author
+
+@Entity
+@Table(name = "books")
+@JsonIgnoreProperties("hibernateLazyInitializer", "handler")
+data class Book(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String,
+
+    @Column(nullable = false, length = 255)
+    val title: String,
+
+    @Column(nullable = false, length = 30, unique = true)
+    val isbn: String,
+
+    @Column(nullable = false, length = 255)
+    val publisher: String,
+
+    @Column(name = "quiz_price", nullable = false)
+    val quizPrice: Int,
+
+    @Column(name = "thumbnail", nullable = true, length = 1000)
+    val thumbnail: String?,
+
+    @ManyToMany
+    @JoinTable(
+        name = "book_author",
+        joinColumns = [JoinColumn(name = "book_id")],
+        inverseJoinColumns = [JoinColumn(name = "author_id")]
+    )
+    val authors: List<Author> = mutableListOf(),
+
+    @ManyToMany
+    @JoinTable(
+        name = "book_translator",
+        joinColumns = [JoinColumn(name = "book_id")],
+        inverseJoinColumns = [JoinColumn(name = "translator_id")]
+    )
+    val translators: List<Translator> = mutableListOf(),
+
+    @ManyToMany
+    @JoinTable(
+        name = "book_illustrator",
+        joinColumns = [JoinColumn(name = "book_id")],
+        inverseJoinColumns = [JoinColumn(name = "illustrator_id")]
+    )
+    val illustrators: List<Illustrator> = mutableListOf()
+)
---
+++ src/main/kotlin/kr/co/bookquiz/api/entity/Illustrator.kt
@@ -0,0 +1,22 @@
+package kr.co.bookquiz.api.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@Entity
+@Table(name = "illustrator")
+@JsonIgnoreProperties("hibernateLazyInitializer", "handler")
+data class Illustrator(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String,
+
+    @Column(unique = true, nullable = false)
+    val name: String
+)
